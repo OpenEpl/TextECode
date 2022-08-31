92f0487c7e977c4f1a8a72d374b269871fa386a5
ArcticLampyrid <ArcticLampyrid@outlook.com>
2022-08-31 18:23:57 +0800

    feature(chocolatey): add metadata package

---
+++ ChocolateyPackages/textecode/config.ps1
@@ -0,0 +1,8 @@
+﻿param ($Version)
+$packageDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$nuspecPath = Join-Path -Path $packageDir -ChildPath 'textecode.nuspec'
+
+[xml]$nuspec = Get-Content $nuspecPath
+$nuspec.package.metadata.version = $Version
+$nuspec.package.metadata.dependencies.dependency.version = "[$Version]"
+$nuspec.save($nuspecPath)
